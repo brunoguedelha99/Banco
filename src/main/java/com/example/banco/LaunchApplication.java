bf76eff3a74b16c194b114aaa11c7c2210469011
brunoguedelha99 <brunoguedelha1999@gmail.com>
2023-01-15 20:14:05 +0000

    interface

--- src/main/java/com/example/banco/LaunchApplication.java
+++ src/main/java/com/example/banco/LaunchApplication.java
@@ -11,7 +11,7 @@ public class LaunchApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(LaunchApplication.class.getResource("Launch.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Banco!");
         stage.setScene(scene);
         stage.show();
